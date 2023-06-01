1a0cb6217f04b580eb2f222c3ebbbee13fd8e857
MatteoLupinacci <matteolupinacci171@gmail.com>
2023-06-01 09:34:50 +0200

    modifica ai metodi del repository

--- src/main/java/com/sdcc/backend_sdcc/repositories/RepositoryDocumento.java
+++ src/main/java/com/sdcc/backend_sdcc/repositories/RepositoryDocumento.java
@@ -20,24 +20,24 @@ public interface RepositoryDocumento extends JpaRepository<Documento,String> {
     List<Documento> findByUtente(Utente utente);
 
     @Query( " select d from Documento d" +
-            " WHERE (d.utente like :utente) and" +
+            " WHERE (d.utente.id like :utente) and" +
             "       (upper(d.tag) like :tag or :tag is null) and" +
             "       (d.anno = :anno or :anno is null) and" +
             "       (d.importo <= :importo or :importo is null)")
     List<Documento> ricercaConFiltri(String tag, Integer anno, Float importo, String utente);
 
     @Query( " select  COALESCE(SUM(d.importo), 0) from Documento d" +
-            " WHERE (d.utente like :utente) and" +
+            " WHERE (d.utente.id like :utente) and" +
             "(d.anno = :anno)")
     Float speseAnno(int anno, String utente);
 
     @Query( " select  COALESCE(SUM(d.importo), 0) from Documento d" +
-            " WHERE (d.utente like :utente) and" +
+            " WHERE (d.utente.id like :utente) and" +
             "(d.tag = :tag)")
     Float spesePerCategoria(String tag, String utente);
 
     @Query( " select COALESCE(SUM(d.importo), 0) from Documento d" +
-            " WHERE (d.utene like :utente) and" +
+            " WHERE (d.utente.id like :utente) and" +
             "(d.anno = :anno) and (d.tag = :tag)")
     Float spesePerCategoria_Anno(int anno, String tag, String utente);
 }
